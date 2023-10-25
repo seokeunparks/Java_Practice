83bc409b86d2befbf0e62f45fedb86937dcea6ee
seokeunparks <pppse620@gmail.com>
2023-10-25 09:59:25 +0900

    Hello

---
+++ Hello.java
@@ -0,0 +1,11 @@
+package com.mystudy.run;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.print("Java");
+        
+
+
+    }
+}
